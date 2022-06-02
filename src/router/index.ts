fd7ec8a13e587fd3b5c79f9b0996f160f847cdff
take-study <1295422420@qq.com>
2022-06-02 08:55:29 +0000

    build(vite tsconfig): alias @
    set @ as alias with src

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import Home from "./views/Home.vue";
+import Home from "@/views/Home.vue";
 
 const routes = [{ path: "/", component: Home }];
 const router = createRouter({
