312b2c52a674336231cad43337283969815613fb
take-study <1295422420@qq.com>
2022-06-13 09:13:45 +0000

    feat(app.tsx ): build main search box

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,32 @@
-import React from "react";
-import "@/assets/sass/app.scss";
+import React, { ChangeEvent, ChangeEventHandler } from "react";
+import "@/styles/app.scss";
 
 export default class App extends React.Component {
+  state: Readonly<{ engine: string }>;
+  constructor(props: Readonly<any>) {
+    super(props);
+    this.state = { engine: "google" };
+  }
+  handleSelect(event: ChangeEvent<HTMLSelectElement>): void {
+    this.setState({ engine: event.target.value });
+  }
   render(): React.ReactNode {
-    return <div className="app">hello</div>;
+    return (
+      <div className="app">
+        <div className="sbox">
+          <div className="search_box">
+            <select
+              onChange={this.handleSelect.bind(this)}
+              defaultValue="google"
+            >
+              <option>google</option>
+              <option>bing</option>
+              <option>baidu</option>
+            </select>
+            <input placeholder={this.state.engine} className="search"></input>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
